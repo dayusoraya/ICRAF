16fa423f3a60687ccf9498e9d94bb20637d4a38d
aekadinata <a.ekadinata@cgiar.org>
2019-09-05 10:33:54 +0700

    to be revised

--- Land_cover.R
+++ Land_cover.R
@@ -3,9 +3,12 @@
 library(raster)
 library(rgdal)
 library(ggplot2)
+library(dplyr)
 
 #Set working directory
-df<- "D:/IO/Papua_Barat/Land_cover/input_file/ICRAF/ICRAF"
+#df<- "D:/IO/Papua_Barat/Land_cover/input_file/ICRAF/ICRAF"
+#AE-WD
+df <- "D:/1_Dsoraya/icraf/"
 setwd(df)
 
 #Writing project file
@@ -13,70 +16,77 @@ setwd(df)
 #Read file from tif files
 #calculate area of land cover classess
 list_of_raster_file <- list.files(df, pattern="\\.tif$")  #type of data character, class=list, 1 column, 11 variables
-length_of_raster<-length(list_of_raster_file)
+length_of_raster <- length(list_of_raster_file)
 
-initial_year<-2018
+initial_year <- 2018
+period <- 3
 #
 land_cov<- read.csv("land_cover.csv", header=TRUE, sep =",")
 for(i in 1:length_of_raster){
 
   eval(parse(text=(paste0("lc", initial_year + (i-1)*3, "<-raster('", list_of_raster_file[i], "')"))))
-  eval(parse(text=(paste0("area_papbar_", initial_year + (i-1)*3, "<-freq(lc", initial_year + (i-1)*3, ")"))))
-  eval(parse(text=(paste0("area_papbar_", initial_year + (i-1)*3, "<- as.data.frame(area_papbar_", initial_year + (i-1)*3, ")"))))
+  eval(parse(text=(paste0("area_papbar_", initial_year + (i-1)*3, " <- freq(lc", initial_year + (i-1)*period, ")"))))
+  eval(parse(text=(paste0("area_papbar_", initial_year + (i-1)*3, " <- as.data.frame(area_papbar_", initial_year + (i-1)*3, ")"))))
   eval(parse(text=(paste0("colnames(area_papbar_", initial_year + (i-1)*3, ")=c('ID', 'lc", initial_year + (i-1)*3, "')"))))
-  eval(parse(text=(paste0("land_cov<-merge(land_cov, area_papbar_", initial_year + (i-1)*3, ", by='ID',all=TRUE)"))))
+  eval(parse(text=(paste0("land_cov <- merge(land_cov, area_papbar_", initial_year + (i-1)*3, ", by='ID',all=TRUE)"))))
   
 }
 
 View(land_cov)
 
 #Subset No Data from the land_cov table
+# AE: first you need to define the ID value of NODATA please consider the following code
+# AE: also consider to use'dplyr' package to help on subsetting dataframe
+# nodatavalue <- 0
+# final_land.cov <- land_cov  %>% filter(ID != nodatavalue)
 final_land.cov<- subset(land_cov[2:20,])
 
 #Load another input files
-land_dist<- read.csv("land_dist.csv", header=FALSE, sep =",")
-land_dist.m<-as.matrix(land_dist)
-int_demand<- read.csv("int_demand.csv", header=FALSE, sep=",")
-add_val<- read.csv("add_value.csv", header=FALSE, sep =",")
-fin_demand<- read.csv("fin_demand.csv", header=FALSE, sep =",")
-sector<-read.csv("sector.csv", header=FALSE, sep =",")
-sector.m<-as.matrix(sector)
-non_lb<- read.csv("non_land_based.csv", header=TRUE, sep = ",")
-rate_grad<-read.csv("gradual_rate.csv", header = FALSE, sep=",")
+land_dist <- read.csv("land_dist.csv", header = FALSE, sep = ",")
+land_dist.m <- as.matrix(land_dist)
+int_demand <- read.csv("int_demand.csv", header = FALSE, sep = ",")
+add_val <- read.csv("add_value.csv", header = FALSE, sep = ",")
+fin_demand <- read.csv("fin_demand.csv", header = FALSE, sep = ",")
+sector <- read.csv("sector.csv", header = FALSE, sep = ",")
+sector.m <- as.matrix(sector)
+non_lb <- read.csv("non_land_based.csv", header = TRUE, sep = ",")
+rate_grad <- read.csv("gradual_rate.csv", header = FALSE, sep = ",")
 
 
 #CALCULATE INVERS LEONTIEF
-int_demand.m<-as.matrix(int_demand)
-add_val.m<-as.matrix(add_val)
-dim<-ncol(int_demand.m)
-int_demand.ctot<-colSums(int_demand.m)
-add_val.ctot<-colSums(add_val.m)
-fin_con<- 1/(int_demand.ctot+add_val.ctot)
-fin_con[is.infinite(fin_con)]<-0
-t.input.invers<-diag(fin_con)
-A<-int_demand.m %*% t.input.invers
-I<-as.matrix(diag(dim))
-I_A<-I-A
-Leontief<-solve(I_A)
+int_demand.m <- as.matrix(int_demand)
+add_val.m <- as.matrix(add_val)
+dim <- ncol(int_demand.m)
+int_demand.ctot <- colSums(int_demand.m)
+add_val.ctot <- colSums(add_val.m)
+fin_con <- 1 / (int_demand.ctot + add_val.ctot)
+fin_con[is.infinite(fin_con)] <- 0
+t.input.invers <- diag(fin_con)
+A <- int_demand.m %*% t.input.invers
+I <- as.matrix(diag(dim))
+I_A <- I - A
+Leontief <- solve(I_A)
 
 
 #Calculation for year 2018
-tot_fin.dem<- rowSums(fin_demand)
-output<-tot_fin.dem+rowSums(int_demand)
-tot_add.val<-colSums(add_val)
-diag_land.cov<- diag(unlist(final_land.cov[,3]))   
-land_req<-land_dist.m %*% diag_land.cov
-LR<- rowSums(land_req)
-prop_GDP<- tot_add.val/output
-prop_inc<- add_val[1,]/output
-prop_profit<- add_val[2,]/output
-FD_prop<- tot_fin.dem/output
-LRC<- LR/output
-LPC<- output/LR
-GDP<- prop_GDP*output
+tot_fin.dem <- rowSums(fin_demand)
+output <- tot_fin.dem + rowSums(int_demand)
+tot_add.val <- colSums(add_val)
+diag_land.cov <- diag(unlist(final_land.cov[, 3])) # AE: What s teh purpose in having "unlist" here?
+land_req <- land_dist.m %*% diag_land.cov
+LR <- rowSums(land_req)
+prop_GDP <- tot_add.val / output
+prop_inc <- add_val[1, ] / output
+prop_profit <- add_val[2, ] / output
+FD_prop <- tot_fin.dem / output
+LRC <- LR / output
+LPC <- output / LR
+GDP <- prop_GDP * output
+
 #transpose
 Income<- t(prop_inc)*output
 Profit<- t(prop_profit)*output
+
 #Include the sectors name
 Results<-cbind(tot_fin.dem,FD_prop,LR,LRC,LPC,output,GDP,Income,Profit)
 
@@ -87,29 +97,33 @@ Results.dat<-as.data.frame(Results)
 
 
 #Looping Simulation from year 2021-2048
-final_table<-NULL
-Results_2<-NULL
-initial_year2<-2018
-for(i in 4:ncol(final_land.cov)) {                      
+final_table <- NULL
+Results_2 <- NULL
+initial_year2 <- 2018
+for(i in 4:ncol(final_land.cov)) {
   #land_cover=final_land.cov[,4:length(final_land.cov)]
-  diag_land.cov2<- diag(unlist(final_land.cov[,i]))   
-  land_req2<-land_dist.m %*% diag_land.cov2
-  LR2<- rowSums(land_req2)
-  FD_sim<-(LR2*LPC)*FD_prop
+  diag_land.cov2 <- diag(unlist(final_land.cov[, i]))
+  land_req2 <- land_dist.m %*% diag_land.cov2
+  LR2 <- rowSums(land_req2)
+  FD_sim <- (LR2 * LPC) * FD_prop
   
   FD_sim[is.nan(FD_sim)] <- 0      #changes
-  Output_sim<-Leontief %*% FD_sim
+  Output_sim <- Leontief %*% FD_sim
   
-  GDP_sim<- Output_sim*prop_GDP
-  Income_sim<- t(prop_inc)*Output_sim
-  Profit_sim<- t(prop_profit)*Output_sim
-  Results_2[[i-3]]<-cbind(Output_sim,GDP_sim,Income_sim,Profit_sim)
+  GDP_sim <- Output_sim * prop_GDP
+  Income_sim <- t(prop_inc) * Output_sim
+  Profit_sim <- t(prop_profit) * Output_sim
+  Results_2[[i - 3]] <-
+    cbind(Output_sim, GDP_sim, Income_sim, Profit_sim)
   
-  names(Results_2[[i-3]])<- c("Output_Sim","GDP_sim","Income_sim","Profit_sim")
-  colnames(Results_2[[i-3]])<- paste(c("Output_sim","GDP_sim","Income_sim","Profit_sim"),2018+(i-3)*3)
+  names(Results_2[[i - 3]]) <-
+    c("Output_Sim", "GDP_sim", "Income_sim", "Profit_sim")
+  colnames(Results_2[[i - 3]]) <-
+    paste(c("Output_sim", "GDP_sim", "Income_sim", "Profit_sim"),
+          2018 + (i - 3) * 3)
   
-  Results_2.dat<-as.data.frame(Results_2)
-  Results_2.m<-as.matrix(Results_2.dat)
+  Results_2.dat <- as.data.frame(Results_2)
+  Results_2.m <- as.matrix(Results_2.dat)
   Results_2.m[is.nan(Results_2.m)] <- 0
   Results_2.m[is.infinite(Results_2.m)] <- 0
   
@@ -121,12 +135,12 @@ for(i in 4:ncol(final_land.cov)) {
   #eval(parse(text=(paste0("Profit", initial_year2 + (i-3)*3, "<- as.data.frame(Profit_sim[",i ,"],)"))))
   
   #eval(parse(text=(paste0("final_table<-cbind(Output", initial_year + (i-1)*3,",","GDP", initial_year + (i-1)*3,",","Income", initial_year + (i-1)*3,",","Profit", initial_year + (i-1)*3,",)"))))
-
-  }
+  
+}
 
 #Combine Output, GDP, Income and Profit into 1 table
 Results_2.dat<-as.data.frame(Results_2.m)
-Final_table<-cbind(sector,Results.dat[,6:9],Results_2.dat)
+Final_table<-cbind(sector,Results.dat[,6:9],Results_2.dat) # AE: this is hardcoded
 
 #Combine results of year 2018 and all those simulation
 all_tables<- cbind(Results.dat,Results_2.dat)
@@ -170,7 +184,7 @@ growth_rate.graph<-ggplot(data=all_growth.rate,aes(x=Tahun, y=GDP_growth_rate))+
 #Calculate economic growth using GDP data only for non land-based sector
 
 #Subset GDP non land-based sector
-nlb_sector<-subset(GDP_tables[18:35,2:12])
+nlb_sector<-subset(GDP_tables[18:35,2:12]) # AE: why you are using GDP from model?
 GDP_nlb<- colSums(nlb_sector)
 
 
@@ -185,7 +199,7 @@ avg_nlb.rate<- mean(c(nlb_2015,nlb_2016,nlb_2017,nlb_2018))
 
 
 #Projection of GDP growth for non land-based sector using linear rate
-growth_nlb.2021=GDP_nlb[2]+(GDP_nlb[2]*avg_nlb.rate)
+growth_nlb.2021=GDP_nlb[2]+(GDP_nlb[2]*avg_nlb.rate) #AE = this is only for one year
 growth_nlb.2024=GDP_nlb[3]+(GDP_nlb[3]*avg_nlb.rate)
 growth_nlb.2027=GDP_nlb[4]+(GDP_nlb[4]*avg_nlb.rate)
 growth_nlb.2030=GDP_nlb[5]+(GDP_nlb[5]*avg_nlb.rate)
@@ -208,7 +222,7 @@ GDP_LR<-subset.data.frame(GDP_tables[1:17,])
 GDP_Lin<-NULL
 for (i in 2:ncol(GDP_tables)) {
   
-  GDPval<-GDP_tables[,i]+(GDP_tables[,i]*0.06*(3*(i-1)))
+  GDPval<-GDP_tables[,i]+(GDP_tables[,i]*0.06*(3*(i-1))) # AE : what is the value of 0.06?
   GDP_Lin[[i-1]]=GDPval
   GDP_Lin[[i-1]]<-cbind(GDPval)
   colnames(GDP_Lin[[i-1]])<- paste(c("GDP_"),2018+(i-2)*3,sep="")
@@ -293,7 +307,7 @@ GDP_papbar<-ggplot(data = GDP_all, aes(x = Year)) +
   geom_line(aes(y = GDP_linear, colour = "GDP_linear")) +
   geom_line(aes(y = GDP_gradual, colour = "GDP_gradual")) +
   scale_colour_manual("", values = c("GDP_baseline"="green", "GDP_linear"="red", "GDP_gradual"="blue")) +
-  xlab("Year") +scale_y_continuous("GDP dalam Rupiah", limits = c(0,500000000)) + labs(title="GDP Papua Barat")
+  xlab("Year") +scale_y_continuous("GDP dalam Rupiah", limits = c(0,1000000000)) + labs(title="GDP Papua Barat")
 
 
 #Calculate economic growth based on each sectors growth
